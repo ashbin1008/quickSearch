159a1ac36371d061e5a1431aba1dc8530f102982
Ashwani Kumar <kumracc@amazon.com>
2019-07-28 11:38:37 +0530

    updated code to open apexclasses

--- force-app/main/default/lwc/quickSearch/quickSearch.js
+++ force-app/main/default/lwc/quickSearch/quickSearch.js
@@ -1,17 +1,54 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, wire, track } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
 import getMetaDataOptions from '@salesforce/apex/QuickSearchController.getMetaDataOptions';
+import getSelectedMetaDataItems from '@salesforce/apex/QuickSearchController.getSelectedMetaDataItems';
 
-export default class QuickSearch extends LightningElement {
+export default class QuickSearch extends NavigationMixin(LightningElement) {
     @wire(getMetaDataOptions) metaDataOptions;
     
+    @wire(getSelectedMetaDataItems, {
+        "selectedMetaDataOption": '$selectedMetaDataOption'
+    }) selectedMetaDataItems;
+    @track selectedMetaDataOption = '';
+
+    @track selectedMetaDataItem = '';
+    url;
+    
     renderedCallback() {
-        var datalist = this.template.querySelector('datalist');
-        var input = this.template.querySelector("input");
-        if(datalist && input) {
-            if (input.getAttribute("list") === datalist.id) {
-                return;
-            }
-            input.setAttribute("list", datalist.id);
+        var datalists = this.template.querySelectorAll("datalist");
+        if (datalists) {
+            datalists.forEach(function(item, index) {
+                item.setAttribute("id", index ? "selectedMetaDataItems" : "metaDataOptions");
+            });
         }
     }
+    
+    selectMetaDataOption(event) {
+        this.selectedMetaDataOption = event.target.value;
+    }
+    
+    selectMetaDataItem(event) {
+        if (this.selectedMetaDataOption === 'apexclass') {
+            this[NavigationMixin.Navigate]({
+                type: 'standard__recordPage',
+                attributes: {
+                    recordId: event.target.value,
+                    actionName: 'view'
+                }
+            });
+        }
+    }
+
+    // navigateToRecordViewPage(event) {
+    //     if (this.selectedMetaDataOption === 'apexclass') {
+    //         let link = "/lightning/setup/ApexClasses/page?address=/" + event.target.value;
+    //         this[NavigationMixin.Navigate]({
+    //             type: "standard__webPage",
+    //             attributes: {
+    //                 "url": link
+    //             }
+    //         });
+    //     //     window.open("/lightning/setup/ApexClasses/page?address=/" + this.selectedMetaDataItem, "_blank");
+    //     }
+    // }
 }
